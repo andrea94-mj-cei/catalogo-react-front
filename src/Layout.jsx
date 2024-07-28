189a526cd7d421765cbc50d52cc4a29973d75f70
Andrea Miguel Jarillo <andreamj94@gmail.com>
2024-07-28 18:40:50 +0200

    commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,39 +1,41 @@
 //Este archivo sustituye al App.jsx, es nuestro archivo principal del documento
 
-
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import './css/index.css'
 import './css/catalogo.css'
+import { BrowserRouter as Router, Route, Link, Routes, NavLink, useParams } from "react-router-dom";
 
-import { createContext, useState } from 'react' //importamos createcontext
+// import { createContext, useState } from 'react' //importamos createcontext
 
-export const ModoOscuroContext = createContext(); //declaramos createcontext
+// import CatalogoDj from './pages/CatalogoDj';
+// import Fecha from './pages/Fecha';
+// import Lugar from './pages/Lugar';
+// import Precio from './pages/Precio';
 
 
 const Layout = () => {
-    const [tema, setTema] = useState("ligth");
-    const nombre = "Andrea";
+    
 
 
     return ( 
-        // <ModoOscuroContext.Provider value={{tema, setTema, nombre}}>
+
         <div>
             {/* Header de nuestro documento */}
             <header>
-                <h1 className="Header-h1">BeatMastersTechno</h1>
+                <h1>BeatMastersTechno</h1>
 
             </header>
-
+            
             <main>
                 <Outlet/>
             </main>
 
-            <button onClick={()=> {
-                setTema(tema=="dark"?"ligth":"dark");
-            }}>Cambiar tema: {tema}</button>
+            
 
             {/* Footer de nuestro documento */}
         </div>
-        // </ModoOscuroContext.Provider>
+        
      );
 }
  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,44 @@
 import { useEffect, useState, useContext } from "react";
-import { ModoOscuroContext } from "../Layout"; //importandolo ya lo podemos utilizar 
+// import { ModoOscuroContext } from "../Layout"; //importandolo ya lo podemos utilizar 
+import data from '/backend/datos.json'; 
 
 const Home = () => {
+    // const [catalogo, setCatalogo] = useState();
+    // const [filter, setFilter] = useState ("");
+    // const [info, setInfo] = useState({
+    //     count: 0,
+    //     next: null,
+    //     prev: null,
+    //     pages: 0
+    // })
+    const [datos, setDatos] = useState([]);
 
-    const tema = useContext(ModoOscuroContext);
+    useEffect(() => {
+      fetch('/backend/datos.json')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => setDatos(data))
+        .catch(error => console.error('Error al cargar el JSON:', error));
+    }, []);
+    
 
 
     return ( 
         <>
-        <h1 className="Home-h1">¿Quieres aprender a producir tu música de mano de los mejores DJ's del momento? {tema}</h1>
-        <button onClick={()=> {
-                setTema(tema=="dark"?"ligth":"dark");
-            }}>Cambiar tema: {tema}</button>
+        <p className="Home-p">¿Quieres aprender a producir tu propia música de mano de los mejores DJ's del momento?</p>
+        <div>
+      <h1>Datos del JSON</h1>
+      <ul>
+        {datos.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+        
         </>
     );
 }
